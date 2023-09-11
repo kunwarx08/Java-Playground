f6060c78f0e32fa5b69fba88a0460403579fc5c2
kunwarx08 <kunwardeep.s.08@gmail.com>
2023-09-12 04:48:40 +0530

    Array biggest and smallest

---
+++ Play/arraySmallestno.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class arraySmallestno {
+     public static void main(String[] args) {
+        int size,i;
+        System.out.println("Enter Size of array");
+        Scanner obj=new Scanner(System.in);
+        size=obj.nextInt();
+       
+        int arr[]=new int[size];
+        System.out.println("Enter Elements of array: ");
+
+        for(i=0;i<size;i++){
+            arr[i]=obj.nextInt();
+        }
+        int small=arr[0];
+        for(i=0;i<size;i++){
+            if(arr[i]<small){
+                small=arr[i];
+            }
+        }
+        System.out.println("Smallest: "+small);
+}
+}
---
+++ Play/arraybiggestno.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class arraybiggestno {
+     public static void main(String[] args) {
+        int size,i;
+        System.out.println("Enter Size of array");
+        Scanner obj=new Scanner(System.in);
+        size=obj.nextInt();
+       
+        int arr[]=new int[size];
+        System.out.println("Enter Elements of array: ");
+
+        for(i=0;i<size;i++){
+            arr[i]=obj.nextInt();
+        }
+        int max=arr[0];
+        for(i=0;i<size;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        System.out.println("Max: "+max);
+}
+}
